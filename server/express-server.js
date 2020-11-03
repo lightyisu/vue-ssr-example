37b5e274c088ff6905c350cf4a28feb2d1c8153b
lightyisu <1513221978@qq.com>
2020-11-03 19:28:43 +0800

    fix the router problem (vueclient 2 mount the component)

--- server/express-server.js
+++ server/express-server.js
@@ -11,7 +11,8 @@ const renderer=require('vue-server-renderer').createBundleRenderer(bundle,{
     clientManifest:clientManifest //通过MANIFEST清单为html模板最后添加客户端激活 使得不需要html-webpack-plugin注入客户端bundle
 })
 
-
+app.use(express.static(path.resolve(__dirname,'../dist')))
+//提供对静态文件的访问 以使得服务端能够找到client.bundle.js 进行服务端激活
 app.get('*',(req,res)=>{
         const context={url:req.url}
         renderer.renderToString(context,(err,html)=>{
@@ -19,8 +20,7 @@ app.get('*',(req,res)=>{
             res.end(html)
         })
 })
-app.use(express.static(path.resolve(__dirname,'../dist')))
-//提供对静态文件的访问 以使得服务端能够找到client.bundle.js 进行服务端激活
+
 app.listen(8080,(err)=>{
     console.log('app running on 8080')
 })
